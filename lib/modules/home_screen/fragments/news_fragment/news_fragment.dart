8c3739d5ca2309f24db2a5b7ef43f90515d7d63f
Ridwan Nur Faisal <114727242+ridwaannf@users.noreply.github.com>
2023-04-15 21:21:58 +0700

    Update news_fragment.dart

--- lib/modules/home_screen/fragments/news_fragment/news_fragment.dart
+++ lib/modules/home_screen/fragments/news_fragment/news_fragment.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../../../../data/data_news.dart';
+
 class NewsFragment extends StatelessWidget {
   const NewsFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("News Fragment"),
+    return Scaffold(
+      body: ListView(
+        children: [
+          Text("Daftar Berita"),
+          Column(
+            children: newslist
+                .map((e) => Card(
+                      child: ListTile(
+                        title: Text(
+                          e.judul,
+                          maxLines: 2,
+                        ),
+                        leading: AspectRatio(
+                            aspectRatio: 16 / 9,
+                            child: Image.network(e.gambar)),
+                        subtitle: Text(
+                          e.deskripsi,
+                          maxLines: 4,
+                        ),
+                      ),
+                    ))
+                .toList(),
+          )
+        ],
       ),
     );
   }
-  
 }
